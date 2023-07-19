b5b277534573dd12acfcbc900a04ff32d7f90ca9
21A91A1221 <97592122+21A91A1221@users.noreply.github.com>
2023-07-19 20:45:12 +0530

    import java.util.Scanner; public class TempConv {     public static void main(String args[])     {         int n;         double f;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         f=(n*1.8)+32;         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TempConv
+{
+    public static void main(String args[])
+    {
+        int n;
+        double f;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        f=(n*1.8)+32;
+        System.out.printf("%.2f",f);
+    }
+}
